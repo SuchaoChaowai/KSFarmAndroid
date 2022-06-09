074b7109916062a81d69170e44ff5e6cf2bbc29c
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 <suchaoc@gmail.com>
2022-06-09 14:21:25 +0700

    QR Code scanner.

--- app/src/main/java/com/cyberello/ksfarm/MainActivity.java
+++ app/src/main/java/com/cyberello/ksfarm/MainActivity.java
@@ -1,14 +1,43 @@
 package com.cyberello.ksfarm;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final ScanOptions options = new ScanOptions();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE);
+        options.setPrompt("Scan a barcode");
+        options.setCameraId(0);  // Use a specific camera of the device
+        options.setBeepEnabled(false);
+        options.setBarcodeImageEnabled(true);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> {
+            barcodeLauncher.launch(options);
+        });
     }
+
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
+            result -> {
+                if (result.getContents() == null) {
+                    Toast.makeText(MainActivity.this, "Cancelled", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            });
 }
