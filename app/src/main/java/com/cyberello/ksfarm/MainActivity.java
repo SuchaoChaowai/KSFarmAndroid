f94bf312827f1313537fe5fde1602d8d497cc379
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 <suchaoc@gmail.com>
2022-06-09 14:21:25 +0700

    QR Code scanner.

--- app/src/main/java/com/cyberello/ksfarm/MainActivity.java
+++ app/src/main/java/com/cyberello/ksfarm/MainActivity.java
@@ -1,177 +1,43 @@
 package com.cyberello.ksfarm;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GestureDetectorCompat;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import com.cyberello.ksfarm.data.KSConstants;
-import com.cyberello.ksfarm.data.json.IOTJSON;
-import com.cyberello.ksfarm.webService.IOTControl;
-
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
 
-import com.cyberello.ksfarm.data.json.IOTJSONWrapper;
-import com.cyberello.ksfarm.data.json.IOTTempJSON;
-import com.cyberello.ksfarm.util.KSFarmUtil;
-import com.cyberello.ksfarm.util.QRCodeUtil;
-import com.cyberello.ksfarm.webService.IOTService;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
-import org.json.JSONObject;
+public class MainActivity extends AppCompatActivity {
 
-import java.text.ParseException;
-
-public class MainActivity extends AppCompatActivity implements QRCodeUtil.QRCodeListener, IOTService.WebServiceResultListener, KSFarmUtil.MetaDataListener {
-
-    private GestureDetectorCompat mDetector;
-
-    private SharedPreferences sharedPreferences;
+    private final ScanOptions options = new ScanOptions();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MyGestureListener myGestureListener = new MyGestureListener();
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE);
+        options.setPrompt("Scan a barcode");
+        options.setCameraId(0);  // Use a specific camera of the device
+        options.setBeepEnabled(false);
+        options.setBarcodeImageEnabled(true);
 
-        mDetector = new GestureDetectorCompat(this, myGestureListener);
+        Button button = findViewById(R.id.button);
 
-        sharedPreferences = this.getSharedPreferences(KSConstants.SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
+        button.setOnClickListener(v -> {
+            barcodeLauncher.launch(options);
+        });
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        this.mDetector.onTouchEvent(event);
-        return super.onTouchEvent(event);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        KSFarmUtil.getIOTMetaJSON(MainActivity.this);
-    }
-
-    public void processQRCodeString(String scannedText) {
-
-        Toast.makeText(MainActivity.this, "Scanned: " + QRCodeUtil.getKSFarmQRString(scannedText), Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void processWebServicePostDataResult(JSONObject response) {
-
-        KSFarmUtil.setLocalIOTData(response.toString(), sharedPreferences);
-    }
-
-    @Override
-    public void processWebServiceGetIOTDataResult(JSONObject response) {
-
-        IOTJSONWrapper iotJSONWrapper = KSFarmUtil.gson().fromJson(response.toString(), IOTJSONWrapper.class);
-
-        KSFarmUtil.setLocalIOTData(response.toString(), sharedPreferences);
-
-        new Thread(() -> iotJSONWrapper.iotJSONs.forEach(this::setIOTData)).start();
-    }
-
-    @Override
-    public void onErrorResponse(String errorMessage) {
-
-
-    }
-
-    @Override
-    public void onErrorResponse(String status, String message) {
-
-    }
-
-    @Override
-    public void processWebServiceGetIOTMetaDataResult(JSONObject response) {
-
-        KSFarmUtil.setIOTMetaData(response, MainActivity.this);
-    }
-
-    private void setIOTData(IOTJSON iotJSON) {
-
-        KSFarmUtil.setIOTData(iotJSON);
-
-        if (iotJSON.name.equals(KSConstants.SECOND_FLOOR_BALCONY)) {
-
-            runOnUiThread(() -> setSecondFloorBalconyTempData(iotJSON));
-        }
-    }
-
-    private void setSecondFloorBalconyTempData(IOTJSON iotJSON) {
-
-        try {
-
-            IOTTempJSON iotTempJSON = KSFarmUtil.gson().fromJson(iotJSON.jsonString, IOTTempJSON.class);
-
-
-            TextView textView = findViewById(R.id.textViewTemp);
-
-            String textString = Math.round(iotTempJSON.temperature) + " C";
-            textView.setText(textString);
-
-            textView = findViewById(R.id.textViewHumid);
-            textString = Math.round(iotTempJSON.humidity) + " %";
-            textView.setText(textString);
-
-            textView = findViewById(R.id.textViewPressure);
-            textString = String.valueOf(Math.round(iotTempJSON.pressure));
-
-            if (textString.length() > 3) {
-
-                textString = textString.charAt(0) + "," + textString.substring(1);
-            }
-
-            textString += " hPa";
-            textView.setText(textString);
-
-            textView = findViewById(R.id.textViewLastUpdate);
-
-            try {
-                textView.setText(KSFarmUtil.getServerDateTimeString(iotJSON.lastUpdateTimeString));
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-
-            textView.setOnClickListener(view -> refreshIOTData(iotJSON));
-
-        } catch (NumberFormatException nex) {
-            nex.printStackTrace();
-        }
-    }
-
-    private void refreshIOTData(IOTJSON iotJSON) {
-
-        IOTControl.refreshIOTData(iotJSON.deviceIP, this, this);
-    }
-
-    @Override
-    public void metaDataReady() {
-
-        IOTService.getIOTData(MainActivity.this, MainActivity.this);
-    }
-
-    @Override
-    public void metaDataEmpty() {
-
-        IOTService.getIOTMetaData(MainActivity.this, MainActivity.this);
-    }
-
-    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-
-        @Override
-        public boolean onDoubleTap(MotionEvent event) {
-
-            IOTService.getIOTData(MainActivity.this, MainActivity.this);
-
-            return true;
-        }
-    }
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
+            result -> {
+                if (result.getContents() == null) {
+                    Toast.makeText(MainActivity.this, "Cancelled", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            });
 }
