70ae68f8fbd932a605bfb540e016670373a4f712
“Suchao <suchaoc@gmail.com>
2022-08-07 19:43:35 +0700

    Add more iot device data.

--- app/src/main/java/com/cyberello/ksfarm/MainActivity.java
+++ app/src/main/java/com/cyberello/ksfarm/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
-import android.util.Log;
-import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
