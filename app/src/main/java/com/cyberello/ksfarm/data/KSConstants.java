0ce3c34e70c26c3a3f415225813b524090109d96
“Suchao <suchaoc@gmail.com>
2022-08-12 20:10:01 +0700

    Add local iot data.

--- app/src/main/java/com/cyberello/ksfarm/data/KSConstants.java
+++ app/src/main/java/com/cyberello/ksfarm/data/KSConstants.java
@@ -2,6 +2,7 @@ package com.cyberello.ksfarm.data;
 
 public class KSConstants {
 
-        public static final String QR_CODE_URL = "https://ksfarm.co/qr/";
-
+    public static final String QR_CODE_URL = "https://ksfarm.co/qr/";
+    public static final String IOT_JSON_WRAPPER = "iotJSONWrapper";
+    public static final String SHARED_PREFERENCES_NAME = "com.cyberello.ksfarm.data";
 }
--- app/src/main/java/com/cyberello/ksfarm/util/KSFarmUtil.java
+++ app/src/main/java/com/cyberello/ksfarm/util/KSFarmUtil.java
@@ -1,6 +1,11 @@
 package com.cyberello.ksfarm.util;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
 import com.cyberello.global.CyberelloConstants;
+import com.cyberello.ksfarm.data.KSConstants;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -24,5 +29,20 @@ public class KSFarmUtil {
     public static String getServerDateTimeString(String dateTimeString) throws ParseException {
 
         return CyberelloConstants.DATE_TIME_FORMAT.format(Objects.requireNonNull(CyberelloConstants.SERVER_DATE_TIME_FORMAT.parse(dateTimeString)));
+    }
+
+    public static void toast(Activity activity, String toastText) {
+
+        Toast.makeText(activity.getApplicationContext(), toastText, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void setLocalIOTData(String iotJSONWrapperString, SharedPreferences sharedPreferences) {
+
+        new Thread(() -> sharedPreferences.edit().putString(KSConstants.IOT_JSON_WRAPPER, iotJSONWrapperString).apply()).start();
+    }
+
+    public static String getLocalIOTData(SharedPreferences sharedPreferences) {
+
+        return sharedPreferences.getString(KSConstants.IOT_JSON_WRAPPER, "");
     }
 }
