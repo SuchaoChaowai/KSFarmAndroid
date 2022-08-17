8c9337c99181a063c7d7eba032d6f9acf60e7179
“Suchao <suchaoc@gmail.com>
2022-08-17 16:28:52 +0700

    Add baro test.

--- app/src/main/java/com/cyberello/ksfarm/data/KSConstants.java
+++ app/src/main/java/com/cyberello/ksfarm/data/KSConstants.java
@@ -15,6 +15,7 @@ public class KSConstants {
     public static final String DESK_TOP_IOT_DEVICE_ID = "DESK_TOP_IOT_DEVICE_ID";
     public static final String SECOND_FLOOR_BALCONY_IOT_DEVICE_ID = "SECOND_FLOOR_BALCONY_IOT_DEVICE_ID";
     public static final String BED_ROOM_TEMP_IOT_DEVICE_ID = "BED_ROOM_TEMP_IOT_DEVICE_ID";
+    public static final String BARO_SENSOR_TEST = "BARO_SENSOR_TEST";
 
     public static final String IOT_WEB_SERVICE_URL = "http://192.168.0.168:8080/KSFarm-Server/iotService";
     public static final String KS_FARM_WEB_SERVICE_URL = "http://192.168.0.168:8080/KSFarm-Server/ksFarmService";
