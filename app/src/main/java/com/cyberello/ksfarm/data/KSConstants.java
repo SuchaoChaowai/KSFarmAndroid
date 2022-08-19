d46c1260b99dfe183a1f4ed33f7d047e95afb37b
“Suchao <suchaoc@gmail.com>
2022-08-19 10:55:13 +0700

    Meta servlet.

--- app/src/main/java/com/cyberello/ksfarm/data/KSConstants.java
+++ app/src/main/java/com/cyberello/ksfarm/data/KSConstants.java
@@ -6,19 +6,19 @@ public class KSConstants {
     public static final String IOT_JSON_WRAPPER = "iotJSONWrapper";
     public static final String SHARED_PREFERENCES_NAME = "com.cyberello.ksfarm.data";
 
-    public static final String FIRST_FLOOR_IOT_DEVICE_ID = "FIRST_FLOOR_IOT_DEVICE_ID";
-    public static final String BED_ROOM_AIR_CON_IOT_DEVICE_ID = "BED_ROOM_AIR_CON_IOT_DEVICE_ID";
-    public static final String OVER_HEAD_DESK_LAMP_IOT_DEVICE_ID = "OVER_HEAD_DESK_LAMP_IOT_DEVICE_ID";
-    public static final String DESK_LAMP_IOT_DEVICE_ID = "DESK_LAMP_IOT_DEVICE_ID";
-    public static final String STANDING_DESK_LAMP_IOT_DEVICE_ID = "STANDING_DESK_LAMP_IOT_DEVICE_ID";
-    public static final String BED_SIDE_LAMP_IOT_DEVICE_ID = "BED_SIDE_LAMP_IOT_DEVICE_ID";
-    public static final String DESK_TOP_IOT_DEVICE_ID = "DESK_TOP_IOT_DEVICE_ID";
-    public static final String SECOND_FLOOR_BALCONY_IOT_DEVICE_ID = "SECOND_FLOOR_BALCONY_IOT_DEVICE_ID";
-    public static final String BED_ROOM_TEMP_IOT_DEVICE_ID = "BED_ROOM_TEMP_IOT_DEVICE_ID";
+    public static final String FIRST_FLOOR = "FIRST_FLOOR";
+    public static final String BED_ROOM_AIR_CON = "BED_ROOM_AIR_CON";
+    public static final String OVER_HEAD_DESK_LAMP = "OVER_HEAD_DESK_LAMP";
+    public static final String DESK_LAMP = "DESK_LAMP";
+    public static final String STANDING_DESK_LAMP = "STANDING_DESK_LAMP";
+    public static final String BED_SIDE_LAMP = "BED_SIDE_LAMP";
+    public static final String DESK_TOP = "DESK_TOP";
+    public static final String SECOND_FLOOR_BALCONY = "SECOND_FLOOR_BALCONY";
+    public static final String BED_ROOM_TEMP = "BED_ROOM_TEMP";
     public static final String BARO_SENSOR_TEST = "BARO_SENSOR_TEST";
     public static final String ESP_01_TEST_DEVICE_001 = "ESP_01_TEST_DEVICE_001";
 
     public static final String IOT_WEB_SERVICE_URL = "http://192.168.0.168:8080/KSFarm-Server/iotService";
     public static final String KS_FARM_WEB_SERVICE_URL = "http://192.168.0.168:8080/KSFarm-Server/ksFarmService";
-    public static final String KS_FARM_IOT_META_URL = "http://192.168.0.168:8080/KSFarm-Server/iot/iot-meta.json";
+    public static final String KS_FARM_IOT_META_URL = "http://192.168.0.168:8080/KSFarm-Server/iot-meta.json";
 }
