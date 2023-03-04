c9ad356a5ee10418a4289f9dfc5dcb81fcdc244b
“Suchao <suchaoc@gmail.com>
2023-03-04 20:09:02 +0700

    Longan menu

--- app/src/main/java/com/cyberello/ksfarm/activity/LonganQountActivity.java
+++ app/src/main/java/com/cyberello/ksfarm/activity/LonganQountActivity.java
@@ -1,8 +1,13 @@
 package com.cyberello.ksfarm.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.cyberello.ksfarm.R;
 
@@ -12,5 +17,40 @@ public class LonganQountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_longan_qount);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.longan_qount_menu, menu);
+
+        super.onCreateOptionsMenu(menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.longanQountIOT) {
+
+            showMainScreen();
+            return true;
+        }
+
+        return false;
+    }
+
+    private void showMainScreen() {
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+
     }
 }
