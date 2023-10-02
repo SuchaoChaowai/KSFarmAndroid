6f05e434e3feb0326e9f3a26c14c424bcce979c0
“Suchao <suchaoc@gmail.com>
2023-10-02 19:43:04 +0700

    Clean up.

--- app/src/main/java/com/cyberello/ksfarm/activity/MainActivity.java
+++ app/src/main/java/com/cyberello/ksfarm/activity/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,17 +22,10 @@ import com.cyberello.ksfarm.KSFarmMeta;
 import com.cyberello.ksfarm.R;
 import com.cyberello.ksfarm.data.KSFarmConstants;
 import com.cyberello.ksfarm.data.LonganQount;
-import com.cyberello.ksfarm.data.json.IOTDataJSON;
-import com.cyberello.ksfarm.data.json.IOTJSON;
-import com.cyberello.ksfarm.data.json.IOTJSONWrapper;
-import com.cyberello.ksfarm.data.json.TemperatureData;
 import com.cyberello.ksfarm.util.KSFarmUtil;
 import com.cyberello.ksfarm.util.QRCodeUtil;
-import com.cyberello.ksfarm.webService.IOTControl;
-import com.cyberello.ksfarm.webService.IOTService;
 import com.cyberello.ksfarm.webService.KSFarmWebService;
 import com.cyberello.ksfarm.webService.OpenWeatherAPI;
-import com.google.android.material.switchmaterial.SwitchMaterial;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -42,7 +34,7 @@ import org.json.JSONObject;
 import java.text.ParseException;
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener, QRCodeUtil.QRCodeListener, KSFarmUtil.MetaDataListener, OpenWeatherAPI.OpenWeatherAPIListener, KSFarmWebService.KSFarmWebServiceResultListener, IOTService.WebServiceResultListener {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener, QRCodeUtil.QRCodeListener, KSFarmUtil.MetaDataListener, OpenWeatherAPI.OpenWeatherAPIListener, KSFarmWebService.KSFarmWebServiceResultListener {
 
     private GestureDetectorCompat mDetector;
     private SharedPreferences sharedPreferences;
@@ -56,8 +48,6 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         mDetector.setOnDoubleTapListener(this);
 
         sharedPreferences = this.getSharedPreferences(KSFarmConstants.SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
-
-        KSFarmMeta.init(MainActivity.this, sharedPreferences);
     }
 
     @Override
@@ -162,7 +152,11 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     @Override
     public void openWeatherDataReady(JSONObject openWeatherJsonObject) {
 
-        setWeatherData(openWeatherJsonObject);
+        try {
+            setWeatherData(openWeatherJsonObject);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -184,107 +178,13 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     }
 
     @Override
-    public void processPostDataResult(JSONObject response) {
-
-
-    }
-
-    @Override
-    public void processGetIOTDataResult(JSONObject response) {
-
-        KSFarmUtil.setLocalIOTData(response.toString(), sharedPreferences);
-
-        IOTJSONWrapper iotJSONWrapper = KSFarmUtil.gson().fromJson(response.toString(), IOTJSONWrapper.class);
-
-        Handler handler = new Handler(Objects.requireNonNull(Looper.myLooper()));
-
-        handler.postDelayed(() -> {
-
-            iotJSONWrapper.iotJSONs.forEach(this::setIOTData);
-            setIOTElementsVisible(View.VISIBLE);
-        }, 100);
-    }
-
-    @Override
     public void onErrorResponse(String errorMessage) {
 
-        setIOTElementsVisible(View.INVISIBLE);
     }
 
     @Override
     public void onErrorResponse(String status, String errorMessage) {
 
-        setIOTElementsVisible(View.INVISIBLE);
-    }
-
-    @Override
-    public void processGetIOTMetaDataResult(JSONObject response) {
-
-        KSFarmUtil.setLocalIOTData(response.toString(), sharedPreferences);
-
-        KSFarmUtil.setIOTMetaData(response, MainActivity.this);
-    }
-
-    private void setIOTData(IOTJSON iotJSON) {
-
-        KSFarmUtil.setIOTData(iotJSON);
-
-        if (iotJSON.name.equals(KSFarmConstants.BED_SIDE_LAMP)) {
-
-            runOnUiThread(() -> setSwitchData(iotJSON, findViewById(R.id.switchBedLamp)));
-            return;
-        }
-
-        if (iotJSON.name.equals(KSFarmConstants.STANDING_DESK_LAMP)) {
-
-            runOnUiThread(() -> setSwitchData(iotJSON, findViewById(R.id.switchStandingDeskLamp)));
-            return;
-        }
-
-        if (iotJSON.name.equals(KSFarmConstants.BED_ROOM_AIR_CON)) {
-
-            runOnUiThread(() -> setSwitchData(iotJSON, findViewById(R.id.switchBedRoomAirCon)));
-        }
-
-        if (iotJSON.name.equals(KSFarmConstants.BED_ROOM_TEMPERATURE)) {
-
-            TextView textViewRoomTemp = findViewById(R.id.textViewRoomTemp);
-
-            TemperatureData temperatureData = KSFarmUtil.gson().fromJson(iotJSON.jsonString, TemperatureData.class);
-
-            String text = temperatureData.temperature + " °C, " + Math.round(temperatureData.humidity) + " %";
-
-            textViewRoomTemp.setText(text);
-
-            return;
-        }
-
-        if (iotJSON.name.equals(KSFarmConstants.SECOND_FLOOR_BALCONY)) {
-
-            TextView textViewOutSideTemp = findViewById(R.id.textViewOutSideTemp);
-
-            TemperatureData temperatureData = KSFarmUtil.gson().fromJson(iotJSON.jsonString, TemperatureData.class);
-
-            String text = temperatureData.temperature + " °C, " + Math.round(temperatureData.humidity) + " %";
-
-            textViewOutSideTemp.setText(text);
-        }
-    }
-
-    private void setSwitchData(IOTJSON iotJSON, SwitchMaterial relaySwitch) {
-
-        try {
-
-            IOTDataJSON iotDataJSON = KSFarmUtil.gson().fromJson(iotJSON.jsonString, IOTDataJSON.class);
-
-            relaySwitch.setChecked(iotDataJSON.relay1.equals("on"));
-
-            relaySwitch.setOnCheckedChangeListener(
-                    (buttonView, isChecked) -> IOTControl.setRelayState(iotJSON.deviceIP, isChecked, MainActivity.this, MainActivity.this));
-
-        } catch (NumberFormatException nex) {
-            nex.printStackTrace();
-        }
     }
 
     private void getWebserviceData() {
@@ -292,39 +192,6 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         Handler handler = new Handler(Objects.requireNonNull(Looper.myLooper()));
 
         handler.postDelayed(() -> OpenWeatherAPI.getOpenWeatherData(MainActivity.this, MainActivity.this), 100);
-
-        KSFarmMeta.getLonganQountDataFromWebService(MainActivity.this, MainActivity.this);
-
-        setIOTElementsVisible(View.INVISIBLE);
-
-        KSFarmUtil.getIOTMetaJSON(MainActivity.this);
-    }
-
-    private void setIOTElementsVisible(int visible) {
-
-        TextView textView = findViewById(R.id.textViewKSFarmIOTLabel);
-
-        textView.setVisibility(visible);
-
-        textView = findViewById(R.id.textViewRoomTemp);
-
-        textView.setVisibility(visible);
-
-        textView = findViewById(R.id.textViewOutSideTemp);
-
-        textView.setVisibility(visible);
-
-        SwitchMaterial relaySwitch = findViewById(R.id.switchBedLamp);
-
-        relaySwitch.setVisibility(visible);
-
-        relaySwitch = findViewById(R.id.switchStandingDeskLamp);
-
-        relaySwitch.setVisibility(visible);
-
-        relaySwitch = findViewById(R.id.switchBedRoomAirCon);
-
-        relaySwitch.setVisibility(visible);
     }
 
     public void processQRCodeString(String scannedText) {
@@ -332,84 +199,75 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         Toast.makeText(MainActivity.this, "Scanned: " + QRCodeUtil.getKSFarmQRString(scannedText), Toast.LENGTH_LONG).show();
     }
 
-    private void setWeatherData(JSONObject openWeatherJsonObject) {
+    private void setWeatherData(JSONObject openWeatherJsonObject) throws JSONException {
 
         JSONArray array;
 
         TextView textView = findViewById(R.id.textViewMainWeather);
 
+        array = openWeatherJsonObject.getJSONArray("weather");
+
+        for (int i = 0; i < array.length(); i++) {
+
+            JSONObject weather = array.getJSONObject(i);
+
+            String main = weather.getString("main") + ", " + weather.getString("description");
+
+            textView.setText(main);
+        }
+
+        JSONObject main = openWeatherJsonObject.getJSONObject("main");
+
+        textView = findViewById(R.id.textViewTemp);
+
+        String textString = Math.round(main.getDouble("temp")) + " °C";
+        textView.setText(textString);
+
+        textView = findViewById(R.id.textViewHumid);
+        textString = Math.round(main.getDouble("humidity")) + " %";
+        textView.setText(textString);
+
+        textView = findViewById(R.id.textViewPressure);
+
+        textString = Math.round(main.getDouble("pressure")) + " hPa";
+        textView.setText(textString);
+
+        JSONObject wind = openWeatherJsonObject.getJSONObject("wind");
+
+        textString = "ลม" + KSFarmUtil.getWindDirection(wind.getDouble("deg"));
+
+        textString = textString + ", " + Math.round(wind.getDouble("speed") * 3.6) + "-" + Math.round(wind.getDouble("gust") * 3.6) + " กม./ชม.";
+
+        textView = findViewById(R.id.textViewWind);
+
+        textView.setText(textString);
+
+        JSONObject sun = openWeatherJsonObject.getJSONObject("sys");
+
+        textString = "พระอาทิตย์  ";
+
+        textString = textString + KSFarmUtil.getSunTime(sun.getLong("sunrise")) + "-" + KSFarmUtil.getSunTime(sun.getLong("sunset"));
+
+        textView = findViewById(R.id.textViewSun);
+
+        textView.setText(textString);
+
         try {
 
-            array = openWeatherJsonObject.getJSONArray("weather");
-
-            for (int i = 0; i < array.length(); i++) {
-
-                JSONObject weather = array.getJSONObject(i);
-
-                String main = weather.getString("main") + ", " + weather.getString("description");
-
-                textView.setText(main);
-            }
-
-            JSONObject main = openWeatherJsonObject.getJSONObject("main");
-
-            textView = findViewById(R.id.textViewTemp);
-
-            String textString = Math.round(main.getDouble("temp")) + " °C";
-            textView.setText(textString);
-
-            textView = findViewById(R.id.textViewHumid);
-            textString = Math.round(main.getDouble("humidity")) + " %";
-            textView.setText(textString);
-
-            textView = findViewById(R.id.textViewPressure);
-
-            textString = Math.round(main.getDouble("pressure")) + " hPa";
-            textView.setText(textString);
-
-            JSONObject wind = openWeatherJsonObject.getJSONObject("wind");
-
-            textString = "ลม" + KSFarmUtil.getWindDirection(wind.getDouble("deg"));
-
-            textString = textString + ", " + Math.round(wind.getDouble("speed") * 3.6) + "-" + Math.round(wind.getDouble("gust") * 3.6) + " กม./ชม.";
-
-            textView = findViewById(R.id.textViewWind);
-
-            textView.setText(textString);
-
-            JSONObject sun = openWeatherJsonObject.getJSONObject("sys");
-
-            textString = "พระอาทิตย์  ";
-
-            textString = textString + KSFarmUtil.getSunTime(sun.getLong("sunrise")) + "-" + KSFarmUtil.getSunTime(sun.getLong("sunset"));
-
-            textView = findViewById(R.id.textViewSun);
-
-            textView.setText(textString);
-
-            try {
-
-                textView = findViewById(R.id.textViewLastUpdate);
-                textView.setText(KSFarmUtil.getServerDateTimeString(openWeatherJsonObject.getLong("dt")));
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-
-        } catch (JSONException e) {
-
-
+            textView = findViewById(R.id.textViewLastUpdate);
+            textView.setText(KSFarmUtil.getServerDateTimeString(openWeatherJsonObject.getLong("dt")));
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
     }
 
     @Override
     public void metaDataReady() {
 
-        IOTService.getIOTData(MainActivity.this, MainActivity.this);
     }
 
     @Override
     public void metaDataEmpty() {
 
-        IOTService.getIOTMetaData(MainActivity.this, MainActivity.this);
     }
 }
