21c0a56d52ca3e41c922ef997538b3c268b16e64
“Suchao <suchaoc@gmail.com>
2023-10-02 20:05:25 +0700

    Add empty fertilizer activity.

--- app/src/main/java/com/cyberello/ksfarm/activity/MainActivity.java
+++ app/src/main/java/com/cyberello/ksfarm/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cyberello.ksfarm.activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
@@ -82,6 +83,8 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
 
     private void processMenu1Selected() {
 
+        Intent intent = new Intent(getApplicationContext(), FertilizerActivity.class);
+        startActivity(intent);
     }
 
     @Override
